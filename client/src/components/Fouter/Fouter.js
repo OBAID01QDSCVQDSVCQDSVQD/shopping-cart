1b6f1ed9f55b90b90e7b1c9e993d8abd7dcc06d6
unknown <abdallah.essafi@gmail.com>
2022-12-22 22:26:49 +0100

    footer-comp

---
+++ client/src/components/Fouter/Fouter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../../css/Fouter/Fouter.css";
+
+
+function  Fouter (){
+
+    return (
+        <salah>
+            Shopping Cart tt 
+        </salah>
+    )
+
+}
+export default Fouter
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,7 +4,28 @@ import "../../css/Header/Header.css"
 function  header (){
 
     return (
-        <header>Shopping Cart</header>
+        <header>
+            <nav className="navbar">
+                <div className="brandtitle">SDK BATIMENT</div>
+                <a href="#" class="togglebutton">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </a>
+                <div className="navbarlinks">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </nav >
+
+
+
+
+        </header>
+        
     )
 
 }
